0d95d45f968f7cbe56d19b56ee107235a78ad1ad
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-20 14:54:30 +0900

    無限リダイレクトの解消

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
-export default function ({ redirect, store }) {
-  if (!store.getters.userSignedIn) {
+export default function ({ route, redirect, store }) {
+  if (route.fullPath !== '/' && !store.getters.userSignedIn) {
     return redirect('/')
   }
 }
