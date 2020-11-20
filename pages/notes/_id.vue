d298bcde12c7fd9741d8f60ce92be18f17001f47
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-20 11:29:18 +0900

    ノートの削除

--- pages/notes/_id.vue
+++ pages/notes/_id.vue
@@ -2,6 +2,12 @@
   <b-container>
     <h1>{{ note.title }}</h1>
     <div class="content" v-html="formatted_content"></div>
+
+    <b-button @click="modalShow = !modalShow">削除</b-button>
+
+    <b-modal v-model="modalShow" title="ノートの削除" @ok="handleDeleteNote"
+      >削除してよろしいですか？</b-modal
+    >
   </b-container>
 </template>
 
@@ -18,6 +24,7 @@ export default Vue.extend({
         title: '',
         content: '',
       } as Note,
+      modalShow: false,
     }
   },
   computed: {
@@ -34,6 +41,23 @@ export default Vue.extend({
         vue.note.title = snapshot.get('title')
         vue.note.content = snapshot.get('content')
       })
+  },
+  methods: {
+    handleDeleteNote(bvModalEvt: any) {
+      bvModalEvt.preventDefault()
+      const vue = this
+
+      db.collection('notes')
+        .doc(this.$route.params.id)
+        .delete()
+        .then(function () {
+          console.log('Note successfully deleted!')
+          vue.$router.push('/')
+        })
+        .catch(function (error) {
+          console.error('Error removing note: ', error)
+        })
+    },
   },
 })
 </script>
