2c1580cc277c32589a122dd80d40be1b595eaee8
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-27 11:59:18 +0900

    feat: タグの付いているノートへのリンク追加

--- components/tags/TagList.vue
+++ components/tags/TagList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .tags.d-flex.justify-content-start
-    b-button(v-for="(tag, index) in tags" :key="index" variant="primary" class="mx-1") {{ tag }}
+    b-button(:to="`/tags/${tag}`" v-for="(tag, index) in tags" :key="index" variant="primary" class="mx-1") {{ tag }}
 </template>
 
 <script lang="ts">
---
+++ pages/tags/_tagId.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  b-container
+    span タグが付いているノート表示
+</template>
