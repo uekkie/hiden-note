30e27191f5d26feeb6df232490bc340c95b3fd54
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-27 10:28:11 +0900

    bug: sassスタイリングのエラー修正

--- pages/index.vue
+++ pages/index.vue
@@ -20,8 +20,7 @@ export default Index
 
 <style lang="sass">
 .title
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
   display: block
   font-weight: 300
   font-size: 100px
