059b57d830fcd0450a241811ba48257cf8f4b5c9
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-30 17:12:00 +0900

    feat: commentsStore実装（未使用）

--- utils/store-accessor.ts
+++ utils/store-accessor.ts
@@ -3,6 +3,7 @@ import { getModule } from 'vuex-module-decorators'
 import Auth from '~/store/auth'
 import Notes from '~/store/notes'
 import Users from '~/store/users'
+import Comments from '~/store/comments'
 
 // eslint-disable-next-line import/no-mutable-exports
 let authStore: Auth
@@ -10,11 +11,14 @@ let authStore: Auth
 let notesStore: Notes
 // eslint-disable-next-line import/no-mutable-exports
 let usersStore: Users
+// eslint-disable-next-line import/no-mutable-exports
+let commentsStore: Comments
 
 function initialiseStores(store: Store<any>): void {
   authStore = getModule(Auth, store)
   notesStore = getModule(Notes, store)
   usersStore = getModule(Users, store)
+  commentsStore = getModule(Comments, store)
 }
 
-export { initialiseStores, authStore, notesStore, usersStore }
+export { initialiseStores, authStore, notesStore, usersStore, commentsStore }
