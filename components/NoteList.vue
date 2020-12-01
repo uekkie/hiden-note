c8c0bcd9fe42e5379e85f43795da2cbdc8cfa126
Hiroshi Ueki <hirocueki@gmail.com>
2020-12-01 17:54:17 +0900

    fix: pug やめる

--- components/NoteList.vue
+++ components/NoteList.vue
@@ -1,27 +1,42 @@
-<template lang="pug">
-  .notes
-    h3 ノート一覧
-    p ノート総数：{{ notes.length }}
+<template>
+  <div class="notes">
+    <h3>ノート一覧</h3>
+    <p>ノート総数：{{ notes.length }}</p>
 
-    b-list-group
-      b-list-group-item(variant="primary"
-        class="flex-column align-items-start")
-        b-row
-          b-col タイトル
-          b-col 作成者
-          b-col 更新日
-
-      b-list-group-item(v-for="(note, index) in notes"
+    <b-list-group>
+      <b-list-group-item
+        variant="primary"
+        class="flex-column align-items-start"
+      >
+        <b-row>
+          <b-col>タイトル</b-col>
+          <b-col>作成者</b-col>
+          <b-col>更新日</b-col>
+        </b-row>
+      </b-list-group-item>
+      <b-list-group-item
+        v-for="(note, index) in notes"
         :key="index"
-        class="flex-column align-items-start")
-        b-row
-          b-col
-            nuxt-link(:to="`/notes/${note.id}`") {{ note.title }}
-          b-col
-            span
-              NuxtLink(:to="`/users/${note.userId}`") {{ userName(note.userId) }}
-          b-col
-            small {{ formatDate(note.updatedAt.toDate()) }}
+        class="flex-column align-items-start"
+      >
+        <b-row>
+          <b-col>
+            <nuxt-link :to="`/notes/${note.id}`">{{ note.title }}</nuxt-link>
+          </b-col>
+          <b-col>
+            <span>
+              <NuxtLink :to="`/users/${note.userId}`">{{
+                userName(note.userId)
+              }}</NuxtLink>
+            </span>
+          </b-col>
+          <b-col>
+            <small>{{ formatDate(note.updatedAt.toDate()) }}</small>
+          </b-col>
+        </b-row>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
