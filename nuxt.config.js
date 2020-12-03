f70f9f6a640a7a02924724596a22406dc84c6f5b
Hiroshi Ueki <hirocueki@gmail.com>
2020-12-03 23:40:26 +0900

    chore

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 const environment = process.env.NODE_ENV || 'development'
-const isDev = environment !== 'production'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const envSet = isDev ? require(`./env.${environment}.ts`) : {}
+const envSet = require(`./env.${environment}.ts`)
 
 export default {
   ssr: false,
