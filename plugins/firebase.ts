f97aedbeb05f8d1371dc6a2c6e484254c52bae4c
Hiroshi Ueki <hirocueki@gmail.com>
2020-12-04 09:03:04 +0900

    fix

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -7,14 +7,6 @@ const config = {
   databaseURL: process.env.firebaseDatabaseURL,
   projectId: process.env.firebaseProjectId,
   storageBucket: process.env.firebaseStorageBucket,
-  // apiKey: 'AIzaSyDtK7TZaHoB03Ei06sxuc1VpuVM5AxAiNM',
-  // authDomain: 'hiden-note.firebaseapp.com',
-  // databaseURL: 'https://hiden-note.firebaseio.com',
-  // projectId: 'hiden-note',
-  // storageBucket: 'hiden-note.appspot.com',
-  // messagingSenderId: '546700661025',
-  // appId: '1:546700661025:web:ce5e2065eca876206fae60',
-  // measurementId: 'G-TJHJSRS8EH',
 }
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
