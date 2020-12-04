148861b4593fb319086f2615ad5214d119931a31
Hiroshi Ueki <hirocueki@gmail.com>
2020-12-04 09:39:35 +0900

    fix

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -2,11 +2,11 @@ import firebase from 'firebase'
 import 'firebase/auth'
 
 const config = {
-  apiKey: process.env.firebaseApiKey,
-  authDomain: process.env.firebaseAuthDomain,
-  databaseURL: process.env.firebaseDatabaseURL,
-  projectId: process.env.firebaseProjectId,
-  storageBucket: process.env.firebaseStorageBucket,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
 }
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
--- utils/algolia.ts
+++ utils/algolia.ts
@@ -1,7 +1,7 @@
 import algoliasearch from 'algoliasearch'
 
 export function algoliaClient(searchKey: string) {
-  const algoliaAppId = process.env.algoliaAppId
+  const algoliaAppId = process.env.ALGOLIA_APP_ID
   const client = algoliasearch(algoliaAppId!, searchKey)
   return client
 }
