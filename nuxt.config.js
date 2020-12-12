49d4650eae6f67022c586f345ffe68fafa77ce1a
Hiroshi Ueki <hirocueki@gmail.com>
2020-12-13 07:23:05 +0900

    コメント削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,8 @@
 const environment = process.env.NODE_ENV || 'development'
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-
 const envSet = require(`./env.${environment}.ts`)
 
 export default {
   ssr: false,
-  // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'hiden-note',
     meta: [
@@ -17,34 +14,17 @@ export default {
   },
   env: envSet,
 
-  // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [
     {
       src: '~/node_modules/highlight.js/styles/atom-one-light.css',
       lang: 'css',
     },
   ],
-
-  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
-
-  // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
+  buildModules: ['@nuxt/typescript-build'],
+  modules: [['bootstrap-vue/nuxt', { icons: true }], '@nuxtjs/markdownit'],
 
-  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-  ],
-
-  // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    // https://go.nuxtjs.dev/bootstrap
-    ['bootstrap-vue/nuxt', { icons: true }],
-    '@nuxtjs/markdownit',
-  ],
-
-  // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     babel: {
       babelrc: false,
