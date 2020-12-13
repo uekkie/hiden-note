1a592d7ded0884115a64137b0bdba3e70be06954
ueki <ueki@uekis-Mac-mini.local>
2020-12-13 17:39:34 +0900

    タグの並びを調整

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,12 +1,16 @@
 <template>
   <b-container>
     <h1 class="title">秘伝のタレ</h1>
-    <div class="m-2">
+    <div class="notes__search-form my-3">
       <note-search-form />
     </div>
     <template v-if="userSignedIn">
-      <tag-index />
-      <note-list />
+      <div class="notes__tag-index">
+        <tag-index />
+      </div>
+      <div class="notes__note-list my-3">
+        <note-list />
+      </div>
     </template>
     <div v-else>ログインしてください</div>
   </b-container>
