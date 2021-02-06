c2c263ea134c75847b9379b5b8f24b023953912d
Hiroshi Ueki <hirocueki@gmail.com>
2021-02-06 17:25:32 +0900

    変更途中

--- pages/notes/_id/edit.vue
+++ pages/notes/_id/edit.vue
@@ -8,38 +8,59 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+// import { Vue, Component } from 'nuxt-property-decorator'
+import {
+  defineComponent,
+  reactive,
+  inject,
+  useAsync,
+} from '@nuxtjs/composition-api'
 import { Note } from '@/models/note'
-import { authStore, notesStore } from '@/store'
+// import { authStore, notesStore } from '@/store'
 import 'highlight.js/styles/atom-one-light.css'
+import NoteKey from '~/composables/use-note-key'
+import { NoteStore } from '~/composables/use-note'
 
-@Component
-class NoteEdit extends Vue {
-  private note: Note | null = null
+export default defineComponent({
+  setup(_props, ctx) {
+    const { getNote } = inject(NoteKey) as NoteStore
+    const noteId = ctx.root.$route.params.id
+    // todo getNoteを実装する
+    const note = useAsync(() => getNote(noteId))
+    return {
+      note,
+    }
+  },
+})
 
-  created() {
-    notesStore.getNote(this.$route.params.id).then((note) => {
-      this.note = note
-    })
-  }
+// @Component
+// class NoteEdit extends Vue {
+//   private note: Note | null = null
 
-  onUpdate(formData: any) {
-    const noteId = this.$route.params.id
+//   created() {
+//     notesStore.getNote(this.$route.params.id).then((note) => {
+//       this.note = note
+//     })
+//   }
 
-    notesStore
-      .updateNote(
-        new Note({
-          id: noteId,
-          userId: authStore.userId,
-          title: formData.title,
-          content: formData.content,
-          tags: formData.tags,
-        })
-      )
-      .then(() => {
-        this.$router.replace({ path: '/notes/' + noteId })
-      })
-  }
-}
-export default NoteEdit
+//   onUpdate(formData: any) {
+//     const noteId = this.$route.params.id
+
+//     notesStore
+//       .updateNote(
+//         new Note({
+//           id: noteId,
+//           userId: authStore.userId,
+//           title: formData.title,
+//           content: formData.content,
+//           tags: formData.tags,
+//         })
+//       )
+//       .then(() => {
+//         this.$router.replace({ path: '/notes/' + noteId })
+//       })
+//   }
+// }
+// export default NoteEdit
+//
 </script>
